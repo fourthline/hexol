d5241d97f8c29c368680fa452c672c91d749301c
fourthline <fourthline@live.jp>
2012-06-09 13:09:33 +0900

    botコード。irc接続維持。

---
+++ config.php
@@ -0,0 +1,22 @@
+<?php
+
+
+define("IRC_CHANNEL", "#TMC" );
+define("IRC_SERVER", "irc.reicha.net" );
+define("IRC_PORT", 6667 );
+
+define("IRC_NICKNAME", "t_hexol" );
+define("IRC_REALNAME", "hexol (i'm bot) ‚½‚ñ‚ç‚é" );
+define("IRC_USERNAME", "hexol" );
+define("PREFIX", "hexol" );
+
+define("BOT_COMMAND_FILE", "command" );
+
+define("USER_ALLOW_CONF", "allow.conf");
+
+define("QUIT_MESSAGE", "Leaving...");
+
+define("BOT_COMMAND_TIMER", 1000);
+define("CONNECTION_TIMER", 120000);
+?>
+
